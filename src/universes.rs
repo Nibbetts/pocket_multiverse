8fde94ca886a19deb0812fe98f76722c72178078
nibbetts <nathanandcritters@gmail.com>
2021-07-10 13:59:07 -0700

    some navigation implementation work

--- src/universes.rs
+++ src/universes.rs
@@ -41,15 +41,116 @@ struct Vertex {
 ///     Its Normal,
 ///     Its vertices,
 ///     Its neighboring triangles by index in Universe,
-///     The equation coefficients of the lines of its edges
-///         (same order as neighbors, each of form z=ax+by+c),
-///     The pairs of vertices for each edge (same order as neighbors),
+///     The line segments of its edges (same order as neighbors)
+///         represented in 2D for navigation across its surface,
+///     The pairs of vertices for each edge (same order as neighbors)
+///         by index in Universe
+/// 
+/// NOTE: The 2D representation of a Triangle is pre-scaled, so that travel
+/// across it is straightforward.
 struct Triangle {
     normal: stl_io::Normal,
     vertices: [usize; 3],
     triangles: [u32; 3],
-    equations: [[f32; 3]; 3],
+    segments: [LineSegment2D; 3],
     edges: [[u32; 2]; 3],
+}
+
+/// A 2D line segment, including slope, intercept,
+/// and upper and lower bounds, each of form y=mx+b, l <= y <= u.
+/// Used to test whether a trajectory passes through it.
+struct LineSegment2D {
+    m: f32, // Slope
+    b: f32, // y-Intercept
+    l: f32, // Lower bound
+    u: f32, // Upper bound
+}
+
+impl LineSegment2D {
+    /// Finds the interction between a Trajectory2D and this segment,
+    /// if it exists, subtracting from the trajectory's length and returning
+    /// how far up the segment from l to u the intersection occurs, scaled to
+    /// the interval [0, 1].
+    /// This replaces coordinates, because each triangle has its own different
+    /// coordinate plane, and 3D coordinates might be unreliable for
+    /// calculating intersections of lines.
+    fn intersect(&self, t: &Trajectory2D) -> Option<(f32, Interp)> {
+        // Intersection: y = m1x + b1 = m2x + b2 -> x = (b2-b1) / (m1-m2)
+        // Occurs at: y = m1(b2-b1)/(m1-m2) + b1
+        if t.m == self.m { None } else {
+            let x = (self.b - t.b) / (t.m - self.m);
+            let y = t.m * x + t.b;
+
+            // Cover all the base cases where no intersection happens
+            if (y < self.l) | (y > self.u) { return None };
+            match t.bound {
+                LineBound::Upper if y > t.y => return None,
+                LineBound::Lower if y < t.y => return None,
+                _ => { // An intersection occurs if we go far enough
+                    // Find the distance to the intersection point
+                    let d = distance2D(x, y, t.x, t.y);
+                    // Find the distance traveled and return it and the
+                    // intersection point as Some Interp between the ends of the
+                    // line segment
+                    Some((d, Interp((y - self.l) / (self.u - self.l))))
+                },
+            }
+        }
+    }
+}
+
+/// An interpolation, or ratio, between two arbitrary things, from [0, 1]
+struct Interp(f32);
+// /// A Point in 2D space. Position, not movement or direction.
+// struct Point2D { x: f32, y: f32 }
+
+/*
+If we can do it all in 3D without having problems when lines barely miss
+because of imperfect calculations and representations, then that would likely
+be faster than converting back and forth between various 2D frames of reference,
+but I'm not sure if we can do so.
+
+Trajectory3D:
+
+[tax, tby, tcz] + [dx, ey, fz], t:[0, T]
+|                 |             |     ^length remaining; must be updated after movement
+|                 |             ^representation of moving from t_0 to T. t must be non-negative
+|                 ^starting point of trajectory; must be updated after movement
+^velocities in each dimention based on trajectory, represented as a vector.
+[a, b, c] must be of unit length, or length gets skewed - unless we want 4D bending of space.
+
+Well, that sort of represents positions. If instead we want to represent velocities,
+length of travel would be harder, but speeds might make more sense? I'm confused.
+*/
+
+fn distance2D(x1: f32, y1: f32, x2: f32, y2: f32) -> f32 {
+    ((x2 - x1).powi(2) + (y2 - y1).powi(2)).powf(0.5)
+}
+
+/// A bound on a line, representing the start of a Trajectory
+enum LineBound {
+    Upper,
+    Lower,
+}
+
+/// A trajectory across a 2D plane, with slope, intercept, upper or lower bound
+/// from which movement originates, and length.
+struct Trajectory2D {
+    m: f32, // Slope
+    b: f32, // y-Intercept
+    bound: LineBound, // Whether bound above or below
+    x: f32, // start position x
+    y: f32, // start position y
+    length: f32, // Distance yet to travel
+}
+
+impl Trajectory2D {
+    /// Moves the trajectory to its end point, changing both start and length
+    /// values. For use within a Triangle, as it has no knowledge of boundaries
+    fn move_to_end(&self) {
+        // TODO: move x and y by length in direction of trajectory
+        self.length = 0.;
+    }
 }
 
 impl Universe {
