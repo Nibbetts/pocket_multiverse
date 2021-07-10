f2b242718ab0fc47d660e295cd700dc493a440fc
nibbetts <nathanandcritters@gmail.com>
2021-07-09 20:09:03 -0700

    adding structs for universe construction

--- src/universes.rs
+++ src/universes.rs
@@ -20,14 +20,47 @@ pub struct Universe {
     // d: f32,
 }
 
+// TODO:  test and consider whether we need a full graph structure implementation
+//  which stores things in Universe by their references instead of by indices. It would
+//  probably nearly double the speed of some operations.
+
+/// A smart vertex which knows:
+///     Its position,
+///     Its neighboring triangles by index in Universe,
+///     Its neighboring vertices by index in Universe,
+///     Its own 'Normal' (as an average of the Normals of the faces around it)
+struct Vertex {
+    position: stl_io::Vertex,
+    triangles: [u32; 3],
+    vertices: [u32; 3],
+    normal: stl_io::Normal,
+    //color: Color, // TODO: for regions in space?
+}
+
+/// A smart triangular face which knows:
+///     Its Normal,
+///     Its vertices,
+///     Its neighboring triangles by index in Universe,
+///     The equation coefficients of the lines of its edges
+///         (same order as neighbors, each of form z=ax+by+c),
+///     The pairs of vertices for each edge (same order as neighbors),
+struct Triangle {
+    normal: stl_io::Normal,
+    vertices: [usize; 3],
+    triangles: [u32; 3],
+    equations: [[f32; 3]; 3],
+    edges: [[u32; 2]; 3],
+}
+
 impl Universe {
     //
 }
 
 /// Loads a universe by name from the model and level directories,
-/// for its 3D data and metadata, respectively. Performs all the 3D geometry
-/// processing necessary to prepare it for orientation and navigation on its
-/// surface.
+/// for its 3D data and metadata, respectively.
+/// 
+/// Performs all the 3D geometry processing necessary to prepare it for
+/// locating and navigating on its surface.
 pub fn load(level_name: &str) -> Universe {
 
     // Load the data from its files
